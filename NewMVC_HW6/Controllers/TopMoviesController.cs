54442ab3963ec6fca74c3091ca919ffac52b09d5
Shane Munoz <Shane.S.Munoz-1@ou.edu>
2019-11-22 11:09:23 -0600

    Add project files.

---
+++ NewMVC_HW6/Controllers/TopMoviesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NewMVC_HW6;
+
+namespace NewMVC_HW6.Controllers
+{
+    public class TopMoviesController : Controller
+    {
+        private DB_128040_practiceEntities db = new DB_128040_practiceEntities();
+
+        // GET: TopMovies
+        public ActionResult Index()
+        {
+            var movies = db.Movies.Include(m => m.Director);
+            var top10 = movies.OrderByDescending(m =>m.gross ).Take(10);
+            return View(top10.ToList());
+        }
+
+        // GET: TopMovies/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movie movie = db.Movies.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+    }
+}
---
+++ NewMVC_HW6/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NewMVC_HW6.MvcApplication" Language="C#" %>
